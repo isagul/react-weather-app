9a2fbc95d45d3e45fab82cb30bae4d56e39ed864
Isa Gul <isa.gul@beyn.com.tr>
2019-05-27 16:07:20 +0300

    weather app structure was created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Downshift from 'downshift';
+import TextField from '@material-ui/core/TextField';
+import cityList from './assets/city.list.json';
+
+let selectedCity = '';
 
 function App() {
+  console.log(cityList);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather Extension</h1>
+      <Downshift
+        onChange={selection => {
+          selectedCity = selection.name;
+          console.log(selectedCity);
+        }}
+        itemToString={item => (item ? item.name : '')}
+      >
+        {({
+          getInputProps,
+          getItemProps,
+          getMenuProps,
+          isOpen,
+          inputValue,
+          selectedItem,
+        }) => (
+            <div>      
+              <TextField
+                id="standard-name"
+                label="City"
+                margin="normal"
+                {...getInputProps()} 
+              />
+              <ul {...getMenuProps()}>
+                {isOpen
+                  ? cityList.slice(0,10000)
+                    .filter(item => !inputValue || item.name.toLowerCase().includes(inputValue.toLowerCase()))
+                    .map((item, index) => (
+                      <li
+                        {...getItemProps({
+                          key: item.id,
+                          index,
+                          item,
+                          style: {
+                            fontWeight: selectedItem === item ? 'bold' : 'normal',
+                          },
+                        })}
+                      >
+                      {item.name}
+                      </li>
+                    ))
+                  : null}
+              </ul>
+            </div>
+          )}
+      </Downshift>      
     </div>
   );
 }
