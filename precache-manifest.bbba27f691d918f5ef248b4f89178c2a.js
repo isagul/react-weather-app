e25bc6dcc933e67a7f5b6a77507c04e6a6c99b06
isagul <isagul3477@gmail.com>
2019-06-19 23:36:14 +0300

    Updates

---
+++ precache-manifest.bbba27f691d918f5ef248b4f89178c2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3c65329c81738042d6af34527f8de6",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "8fb1e42c21f1d339ce77",
+    "url": "/react-weather-app/static/css/main.cb8c14f2.chunk.css"
+  },
+  {
+    "revision": "5120b2595cf5d9708c94",
+    "url": "/react-weather-app/static/js/2.5cf31cf0.chunk.js"
+  },
+  {
+    "revision": "8fb1e42c21f1d339ce77",
+    "url": "/react-weather-app/static/js/main.b95e4e20.chunk.js"
+  },
+  {
+    "revision": "e736fc5812ad96aa92b3",
+    "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
+  }
+]);
